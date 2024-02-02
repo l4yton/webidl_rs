a06194a69e275537512c756ea59934e6afa30ac7
l4yton <l4yton0@protonmail.com>
2024-02-02 19:37:11 +0100

    undo last commit

--- src/internal.rs
+++ src/internal.rs
@@ -1,5 +1,5 @@
-#[cfg(feature = "hstr-atom")]
+#[cfg(feature = "swc-atoms")]
 pub type String = hstr::Atom;
 
-#[cfg(not(feature = "hstr-atom"))]
+#[cfg(not(feature = "swc-atoms"))]
 pub type String = std::string::String;
