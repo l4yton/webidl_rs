b0f11ae069b22d584e4197fdf9c0b85c0cf959c6
l4yton <l4yton0@protonmail.com>
2024-02-02 19:19:37 +0100

    update hstr dependency

--- src/internal.rs
+++ src/internal.rs
@@ -1,5 +1,5 @@
-#[cfg(feature = "swc-atoms")]
-pub type String = swc_atoms::Atom;
+#[cfg(feature = "hstr-atom")]
+pub type String = hstr::Atom;
 
-#[cfg(not(feature = "swc-atoms"))]
+#[cfg(not(feature = "hstr-atom"))]
 pub type String = std::string::String;
